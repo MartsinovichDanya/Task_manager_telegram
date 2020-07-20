1316359ccb83d06d0b49c2cf661c4b109fa7ccd3
maxcore25 <mkorobov2002@mail.ru>
2020-07-20 22:12:32 +0300

    main bot file

--- bot.py
+++ bot.py
@@ -215,34 +215,11 @@ from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 
 def start(bot, update):
-    update.message.reply_text('Привет! ', reply_markup=markup)
+    update.message.reply_text('Вас приветствует командный Таскменеджер. В качестве теста нажмите: \n 1 - Вы босс \n 2 - Вы сотрудник', reply_markup=markup)
 
 
 def close_keyboard(bot, update):
-    update.message.reply_text('Ok', reply_markup=ReplyKeyboardRemove())
-
-
-def echo(bot, update):
-    if update.message.text[-1] == '?':
-        update.message.reply_text('Конечно можно спросить! Только я культурно промолчу...')
-    else:
-        update.message.reply_text('Вполне возможно, кто ж знает?')
-
-
-def address(bot, update):
-    update.message.reply_text('Адрес: Китай, Гималаи, хребет Махалангур-Химал, вершина Эверест, д. 1')
-
-
-def phone(bot, update):
-    update.message.reply_text('Телефон: +86 133 2686 8519')
-
-
-def site(bot, update):
-    update.message.reply_text('Сайт: https://yandex.ru/everest/')
-
-
-def work_time(bot, update):
-    update.message.reply_text('Время работы: пн-пт, 9-00 - 19-00')
+    update.message.reply_text('Закрываю клавиатуру', reply_markup=ReplyKeyboardRemove())
 
 
 def echo(bot, update):
@@ -254,19 +231,13 @@ updater = Updater(TOKEN)
 # Получаем из него диспетчер сообщений.
 dp = updater.dispatcher
 
-reply_keyboard = [['/address', '/phone'],
-                  ['/site', '/work_time'],
-                  ['начать']]
+reply_keyboard = [['закрыть клавиатуру', 'начать']]
 
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 # Регистрируем обработчик команды "start" в диспетчере
 dp.add_handler(CommandHandler('start', start))
-dp.add_handler(CommandHandler('close', close_keyboard))
-dp.add_handler(CommandHandler('address', address))
-dp.add_handler(CommandHandler('phone', phone))
-dp.add_handler(CommandHandler('site', site))
-dp.add_handler(CommandHandler('work_time', work_time))
+dp.add_handler(MessageHandler(Filters.regex('закрыть клавиатуру'), close_keyboard))
 dp.add_handler(MessageHandler(Filters.regex('начать'), start))
 
 # Создаём обработчик текстовых сообщений типа Filters.text
