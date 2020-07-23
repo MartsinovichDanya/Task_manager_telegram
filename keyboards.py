04a2093d4555d3c9f2e53beda3401482de2d5333
maxcore25 <mkorobov2002@mail.ru>
2020-07-23 22:03:49 +0300

    keyboards

---
+++ keyboards.py
@@ -0,0 +1,43 @@
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from DB import DB
+from Models import UserModel, TaskModel, ProjectModel, EmployeeModel
+
+
+# Клавиатура Босса
+def create_boss_keyboard1():
+    boss_reply_keyboard1 = [['Просмотр по проектам', 'Просмотр по сотрудникам', 'Редактирование']]
+    boss_markup1 = ReplyKeyboardMarkup(boss_reply_keyboard1, one_time_keyboard=False)
+    return boss_markup1
+
+
+def create_boss_keyboard2():
+    boss_reply_keyboard2 = [['Добавить проект', 'Добавить задачу', 'Добавить сотрудника'],
+                            ['Удалить проект', 'Удалить задачу', 'Удалить сотрудника']]
+    boss_markup2 = ReplyKeyboardMarkup(boss_reply_keyboard2, one_time_keyboard=False)
+    return boss_markup2
+
+
+def create_boss_keyboard3():
+    boss_reply_keyboard3 = [['Проект1', 'Проект2', 'Проект3']]
+    boss_markup3 = ReplyKeyboardMarkup(boss_reply_keyboard3, one_time_keyboard=False)
+    return boss_markup3
+
+
+# Клавиатура сотрудника
+def create_employee_keyboard1():
+    employee_reply_keyboard1 = [['Просмотр задач']]
+    employee_markup1 = ReplyKeyboardMarkup(employee_reply_keyboard1, one_time_keyboard=False)
+    return employee_markup1
+
+
+def create_employee_keyboard2():
+    employee_reply_keyboard2 = [['Задача1', 'Задача2', 'Задача3']]
+    employee_markup2 = ReplyKeyboardMarkup(employee_reply_keyboard2, one_time_keyboard=False)
+    return employee_markup2
+
+
+def create_employee_keyboard3():
+    employee_reply_keyboard2 = [['Выполнено']]
+    employee_markup2 = ReplyKeyboardMarkup(employee_reply_keyboard2, one_time_keyboard=False)
+    return employee_markup2
