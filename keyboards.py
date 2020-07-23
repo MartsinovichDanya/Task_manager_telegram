a0a12a7408bd0ad9599757ce4a0c41b86c8e6f2a
danya <den.martsinovich@yandex.ru>
2020-07-23 22:25:35 +0300

    dynamic keyboard update boss projects

--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,5 @@
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
-from DB import DB
 from Models import UserModel, TaskModel, ProjectModel, EmployeeModel
 
 
@@ -18,8 +17,10 @@ def create_boss_keyboard2():
     return boss_markup2
 
 
-def create_boss_keyboard3():
-    boss_reply_keyboard3 = [['Проект1', 'Проект2', 'Проект3']]
+def create_boss_keyboard3(db):
+    pm = ProjectModel(db.get_connection())
+    projects = [el[1] for el in pm.get_all()]
+    boss_reply_keyboard3 = [projects]
     boss_markup3 = ReplyKeyboardMarkup(boss_reply_keyboard3, one_time_keyboard=False)
     return boss_markup3
 
