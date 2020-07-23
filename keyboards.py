45e6681ee9750cd73399b42f5ec5fea81afea11d
maxcore25 <mkorobov2002@mail.ru>
2020-07-23 22:20:43 +0300

    keyboards

--- keyboards.py
+++ keyboards.py
@@ -5,39 +5,39 @@ from Models import UserModel, TaskModel, ProjectModel, EmployeeModel
 
 
 # Клавиатура Босса
-def create_boss_keyboard1():
+def create_main_boss_keyboard():
     boss_reply_keyboard1 = [['Просмотр по проектам', 'Просмотр по сотрудникам', 'Редактирование']]
     boss_markup1 = ReplyKeyboardMarkup(boss_reply_keyboard1, one_time_keyboard=False)
     return boss_markup1
 
 
-def create_boss_keyboard2():
+def create_edit_boss_keyboard():
     boss_reply_keyboard2 = [['Добавить проект', 'Добавить задачу', 'Добавить сотрудника'],
                             ['Удалить проект', 'Удалить задачу', 'Удалить сотрудника']]
     boss_markup2 = ReplyKeyboardMarkup(boss_reply_keyboard2, one_time_keyboard=False)
     return boss_markup2
 
 
-def create_boss_keyboard3():
+def create_projects_boss_keyboard():
     boss_reply_keyboard3 = [['Проект1', 'Проект2', 'Проект3']]
     boss_markup3 = ReplyKeyboardMarkup(boss_reply_keyboard3, one_time_keyboard=False)
     return boss_markup3
 
 
 # Клавиатура сотрудника
-def create_employee_keyboard1():
+def create_preview_employee_keyboard():
     employee_reply_keyboard1 = [['Просмотр задач']]
     employee_markup1 = ReplyKeyboardMarkup(employee_reply_keyboard1, one_time_keyboard=False)
     return employee_markup1
 
 
-def create_employee_keyboard2():
+def create_tasks_employee_keyboard():
     employee_reply_keyboard2 = [['Задача1', 'Задача2', 'Задача3']]
     employee_markup2 = ReplyKeyboardMarkup(employee_reply_keyboard2, one_time_keyboard=False)
     return employee_markup2
 
 
-def create_employee_keyboard3():
-    employee_reply_keyboard2 = [['Выполнено']]
-    employee_markup2 = ReplyKeyboardMarkup(employee_reply_keyboard2, one_time_keyboard=False)
-    return employee_markup2
+def create_done_employee_keyboard():
+    employee_reply_keyboard3 = [['Выполнено']]
+    employee_markup3 = ReplyKeyboardMarkup(employee_reply_keyboard3, one_time_keyboard=False)
+    return employee_markup3
