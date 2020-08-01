56ef4e08a29d76eec821faf841ba6ff1b71ee5db
danya <den.martsinovich@yandex.ru>
2020-08-01 21:38:42 +0300

    boss commands update

--- boss_commands.py
+++ boss_commands.py
@@ -1,30 +1,20 @@
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from DB import DB
 from Models import UserModel, TaskModel, ProjectModel, EmployeeModel
 
 db = DB('tm.db')
 
 
-def add_project(bot, update):
-    name = ''  # получить от пользователя
-
+def add_project(name):
     pm = ProjectModel(db.get_connection())
     pm.insert(name)
 
 
-def add_task(bot, update):
-    name, description = '', ''  # получить от пользователя
-    emp_id, project_id = 0, 0  # получить от пользователя
-
+def add_task(name, description, emp_id, project_id):
     tm = TaskModel(db.get_connection())
     tm.insert(name, description, emp_id, project_id)
 
 
-def add_employee(bot, update):
-    name = ''  # получить от пользователя
-    id = 0  # получить от пользователя
-
+def add_employee(name, id):
     um = UserModel(db.get_connection())
     um.insert(id, name)
 
@@ -32,22 +22,16 @@ def add_employee(bot, update):
     em.auto_update()
 
 
-def delete_project(bot, update):
-    name = ''  # получить от пользователя
-
+def delete_project(name):
     pm = ProjectModel(db.get_connection())
     pm.delete(pm.get_id(name))
 
 
-def delete_task(bot, update):
-    id = 0  # получить от пользователя
-
+def delete_task(id):
     tm = TaskModel(db.get_connection())
     tm.delete(id)
 
 
-def delete_employee(bot, update):
-    id = 0  # получить от пользователя
-
+def delete_employee(id):
     em = EmployeeModel(db.get_connection())
     em.delete(id)
