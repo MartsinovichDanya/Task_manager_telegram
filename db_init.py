a80d5a69520de7756c557275918a9b54cbc14e64
maxcore25 <mkorobov2002@mail.ru>
2020-08-26 22:01:54 +0300

    useless changes

--- db_init.py
+++ db_init.py
@@ -7,9 +7,9 @@ def init_tm_db():
     um = UserModel(db.get_connection())
     um.init_table()
     # um.insert(394406731, 'Danya')
-    um.insert(1027909953, 'Maxim', boss=True)
-    # um.insert(1579583, 'Евгений Викторович', boss=True)
-    um.insert(60880374, 'Виталий Викторович', boss=True)
+    um.insert(1027909953, 'Maxim')
+    um.insert(1579583, 'Евгений Викторович', boss=True)
+    # um.insert(60880374, 'Виталий Викторович', boss=True)
 
     pm = ProjectModel(db.get_connection())
     pm.init_table()
