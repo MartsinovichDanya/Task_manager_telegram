a86e2f354ef1b4ab5f5d443121b7f1f2376dd54a
maxcore25 <mkorobov2002@mail.ru>
2020-09-10 22:39:33 +0300

    useless change

--- db_viewer.py
+++ db_viewer.py
@@ -5,22 +5,22 @@ from DB import DB
 def view_tm_db():
     db = DB('tm.db')
     um = UserModel(db.get_connection())
-    print(um.get_all())
+    print('Пользователи: ', um.get_all())
 
     print('-----------------------------------------------')
 
     pm = ProjectModel(db.get_connection())
-    print(pm.get_all())
+    print('Проекты: ', pm.get_all())
 
     print('-----------------------------------------------')
 
     em = EmployeeModel(db.get_connection())
-    print(em.get_all())
+    print('Сотрудники: ', em.get_all())
 
     print('-----------------------------------------------')
 
     tm = TaskModel(db.get_connection())
-    print(tm.get_all())
+    print('Задачи: ', tm.get_all())
 
 
 view_tm_db()
