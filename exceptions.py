cd7f81b06c7dcd8644caf2a42470d829faf519e6
danya <den.martsinovich@yandex.ru>
2020-11-01 13:35:00 +0300

    vernul nah

---
+++ exceptions.py
@@ -0,0 +1,18 @@
+class UserNotFound(Exception):
+    pass
+
+
+class ProjectNotFound(Exception):
+    pass
+
+
+class ProjectAlreadyExist(Exception):
+    pass
+
+
+class UserAlreadyExist(Exception):
+    pass
+
+
+class TaskNotFound(Exception):
+    pass
