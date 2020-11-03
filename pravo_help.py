9f3ac5496b7f343fb00b304ba746bab8650d85ec
danya <den.martsinovich@yandex.ru>
2020-11-04 00:50:03 +0300

    kpz db created

--- pravo_help.py
+++ pravo_help.py
@@ -1,24 +1,4 @@
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
-from telegram import ReplyKeyboardRemove
-
-from datetime import datetime
-
-from DB import DB
-from Models import UserModel, TaskModel, ProjectModel, EmployeeModel
-from keyboards import create_employee_boss_keyboard
-from keyboards import create_main_employee_keyboard
-from keyboards import create_tasks_in_project_boss_keyboard
-from keyboards import create_report_boss_keyboard
-from keyboards import create_report_projects_boss_keyboard, create_report_employee_boss_keyboard
-from keyboards import create_report_task_boss_keyboard
-
-from commands import add_project, add_task, add_employee, delete_project, delete_task, delete_employee, set_done
-from commands import all_task_report, emp_report, proj_report
-
-from exceptions import UserNotFound, UserAlreadyExist, ProjectNotFound, ProjectAlreadyExist, TaskNotFound
-
-import requests
-
 from keyboards import create_main_pravo_help_keyboard, create_payment_pravo_help_keyboard, create_menu_keyboard
 
 import os
@@ -29,6 +9,8 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 TOKEN = os.getenv('TOKEN2')
+
+
 
 
 # Приветствие
