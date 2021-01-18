dd059c34ca687950f263d616da50ce018b31b7a3
maxcore25 <mkorobov2002@mail.ru>
2021-01-19 00:39:06 +0300

    New bot and so

--- email_test.py
+++ email_test.py
@@ -1,5 +1,5 @@
 from commands import send_email
 
-FILE = 'AN_1_SEM_TR.pdf'
+FILE = '2ИКБО-14-20.xlsx'
 # send_email('den.martsinovich@yandex.ru', '<h2>test email</h2>', file=FILE)
-send_email('den.martsinovich@yandex.ru', '<h2>test email</h2>')
+send_email('den.martsinovich@yandex.ru', '<h2>ПАШОЛ НАААААААААААААХ</h2>', file=FILE)
